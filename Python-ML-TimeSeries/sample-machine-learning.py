54a2daf42f8ca767f86a226df76fd813843162b9
hal255 <hluu114@gmail.com>
2018-12-07 23:26:01 -0800

    Added comments for naive approach

--- Python-ML-TimeSeries/sample-machine-learning.py
+++ Python-ML-TimeSeries/sample-machine-learning.py
@@ -12,47 +12,34 @@ class TimeSeriesNaive:
 
     # import csv
     def get_csv(self, csv_file):
-        return pd.read_csv(csv_file)
+        result = pd.read_csv(csv_file)
+        return result
 
     def get_training(self, data_file):
-        return data_file[0:10392]
+        result = data_file[0:10392]
+        return result
 
     def get_training_test(self, data_file):
-        return data_file[10392:]
+        result = data_file[10392:]
+        return result
 
     def get_y_hat(self, training, training_test):
         dd = np.asarray(training.Count)
         y_hat = training_test.copy()
+
+        # Naive approach: set value of y_hat to previous day of y_hat
+        # set all y_hat['naive'] values to last value of training['Count']
         y_hat['naive'] = dd[len(dd) - 1]
+
         return y_hat
-
-
-class TimeSeriesML:
-    def __init__(self):
-        self.alpha = 0
-        self.level_0 = 0
-        self.seasonal_0 = 0
-        self.trend_0 = 0
-
-        self.level = self.update_level(self.alpha)
-        self.trend = None
-        self.seasonal = None
-        self.forecast = None
-
-    def update_level(self, alpha):
-        self.level = 1
-
-    def get_level(self):
-        return self.level
-
 
 
 if __name__ == '__main__':
     naive_test = TimeSeriesNaive('train.csv')
-    plt.figure(figsize=(12,8))
-    plt.plot(naive_test.train.index, naive_test.train['Count'], label='train')
-    plt.plot(naive_test.training_test.index, naive_test.training_test['Count'], label='training_test')
-    plt.plot(naive_test.y_hat.index, naive_test.y_hat['naive'], label='naive forecast')
+    plt.figure(figsize=(12, 8))     # size of view window
+    plt.plot(naive_test.train.index, naive_test.train['Count'], label='train')                  # display train values
+    plt.plot(naive_test.training_test.index, naive_test.training_test['Count'], label='training_test')  # display test
+    plt.plot(naive_test.y_hat.index, naive_test.y_hat['naive'], label='naive forecast')         # display naive values
     plt.legend(loc='best')
     plt.title('Naive forecast')
     plt.show()
